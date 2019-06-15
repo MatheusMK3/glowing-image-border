c3b60074df4fec3c025902e511c50b1c2c9c948f
Matheus Pratta <matpratta@gmail.com>
2019-06-15 00:23:50 -0300

    Step 2 #wip

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ App = ($ => {
     const $app = $('#app')
     const $main = $('main').first()
     const $header = $('header').first()
-    const $sections = $main.children('section')
+    const $footer = $('footer').first()
+    const $sections = $main.children('.container').children('section')
 
     // Current screen
     let currentStep = 0
@@ -56,21 +57,67 @@ App = ($ => {
     // Initialize
     this.goto(currentStep)
 
-    // Creates a image variable, preview, dropzone and it's handler
+    // Creates a image variable, croppie, dropzone and it's handler
     let image = null
-    let imagePreview = $('#file-select')
+    let imageCroppie = null
     let dropzone = new Dropzone ('#file-select', {
         url: document.location.href,
         autoProcessQueue: false,
         addedfile: file => console.log,
         accept: file => {
+            // Loads our object URL and loads into the cropping box
             image = new Image()
             image.onload = () => {
+                // Load Croppie
+                let viewportSize = $main.outerHeight() / 3
+                imageCroppie = new Croppie($editorPreview[0], {
+                    viewport: {
+                        type: 'circle',
+                        width: viewportSize,
+                        height: viewportSize
+                    },
+                    points: [0, 0, image.width, image.height]
+                })
+
+                // Next screen
                 this.next()
-                // imagePreview.css({ backgroundImage: `url(${image.src})` })
+
+                // Show footer
+                setTimeout(() => {
+                    $footer.addClass('visible')
+                }, 350)
             }
+
+            // Start loading image
             image.src = URL.createObjectURL(file)
+            $editorPreview = $('#editor-crop').attr('src', image.src)
         },
+    })
+
+    // Finishes the cropping phase
+    let croppedImage = null
+    $('#btn-finish-crop').on('click', () => {
+        // Gets the result of the cropped image
+        imageCroppie.result('rawcanvas', {
+            width: 512,
+            height: 512
+        }, 'png', true)
+            .then(croppedImage => {
+                // Appends the canvas to the header, where it will be our preview
+                $header.append(croppedImage)
+
+                // Hides the cropper
+                $footer.removeClass('visible')
+
+                // Animation wait
+                setTimeout(() => {
+                    // Goes to next page
+                    this.next()
+
+                    // Another animation... then we show the header where the final result will be previewed
+                    setTimeout(() => { $header.addClass('visible') }, 350)
+                }, 350)
+            })
     })
 
     // Returns the api
