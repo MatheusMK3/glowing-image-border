9fbf1133be34f6d1ec61ec993df64d34172537ef
Matheus Pratta <matpratta@gmail.com>
2019-06-15 03:52:23 -0300

    Fixed build script, replaced absolute paths with relatives, improved match replace to preserve binary files

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ const map_copy = [
 
 // File rewrites
 const rewrites = [
-    { find: /node_modules/gi, repl: 'vendor' },
+    { on: ['html', 'js', 'css'], find: /node_modules/gi, repl: 'vendor' },
 ]
 
 // Make a new dist directory
 if (!fs.existsSync('dist'))
     fs.mkdirSync('dist')
+
+// Processes rewrites extension list
+let rewrites_allowed = []
+rewrites.map(rewrite => {
+    rewrites_allowed = rewrites_allowed.concat(rewrite.on)
+    return rewrite
+})
 
 // Processes copy list
 map_copy.map(copy => walkFileCopy(copy.src, `dist/${copy.dst}`))
@@ -36,17 +43,25 @@ function walkFileCopy (file, dest) {
             let files = fs.readdirSync(file)
             files.map(subFile => walkFileCopy(`${file}/${subFile}`, `${dest}/${subFile}`))
         } else {
-            // Opens current file
-            let fileContent = fs.readFileSync(file).toString()
-            
-            // Processes rewrites
-            rewrites.map(rewrite => {
-                console.log(rewrite)
-                fileContent = fileContent.replace(rewrite.find, rewrite.repl)
-            })
+            // Gets file extension
+            let extension = file.split('.').reverse()[0]
 
-            // Writes new file
-            fs.writeFileSync(dest, fileContent)
+            // Test if file extension is on the extensions allowed for rewrite (requires non-binary copy mode)
+            if (~rewrites_allowed.indexOf(extension)) {
+                // Opens current file
+                let fileContent = fs.readFileSync(file).toString()
+                
+                // Processes rewrites
+                rewrites.map(rewrite => {
+                    fileContent = fileContent.replace(rewrite.find, rewrite.repl)
+                })
+
+                // Writes new file
+                fs.writeFileSync(dest, fileContent)
+            } else {
+                // Standard copy
+                fs.copyFileSync(file, dest)
+            }
         }
     } catch (e) {
         console.error(`ERROR:`, e)
